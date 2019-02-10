c5310b80d7c1f59b009b248af85b588468542ae2
Ezequiel Monforte <ezequielmonforte@gmail.com>
2019-02-09 21:17:16 -0300

    button order added

--- listaNrosDecimales/main.js
+++ listaNrosDecimales/main.js
@@ -6,7 +6,7 @@ const BT_order= document.getElementById('BT_order')
 
 var numEntered= [];
 
-BT_order.setAttribute('direction', '')
+BT_order.setAttribute('direction', 'desc')
 
 EL_entry.addEventListener('keypress', (event)=> {
     if(event.key === 'Enter'){
@@ -36,8 +36,21 @@ BT_sum.addEventListener('click', ()=>{
     EL_show.innerHTML= `Se sumaron: ${numEntered.length} numeros y el resultado es: <strong>${resultado}</stong>`
 })
 
-BT_order.addEventListener('click', (event)=>{
-
+BT_order.addEventListener('click', ()=>{
+    let aux= numEntered.slice()
+    const orderAsc= 'Ordenar -+'
+    const orderDesc= 'Ordenar +-'
+    if(BT_order.getAttribute('direction')==='desc'){
+        aux.sort(function compare(a,b){return b-a})
+        BT_order.setAttribute('direction', 'asc')
+        BT_order.innerHTML= orderAsc
+    }
+    else if(BT_order.getAttribute('direction')==='asc'){
+        aux.sort(function compare(a,b){return a-b}) 
+        BT_order.setAttribute('direction', 'desc')   
+        BT_order.innerHTML= orderDesc
+    }
+    EL_show.innerHTML= aux.slice(0,4)+ '<br/>'+ aux
 })
 
 
