ee44d1db1916d8b24ff6d885709f832e2a5d1415
Ezequiel Monforte <Ezequielmonforte@gmail.com>
2019-04-01 23:03:45 -0300

    nd

--- Clase2/Clase2/MainPage.xaml.cs
+++ Clase2/Clase2/MainPage.xaml.cs
@@ -25,26 +25,43 @@ namespace Clase2
     /// </summary>
     public sealed partial class MainPage : Page
     {
+		
 		FileOpenPicker seleccionadorImagen;
 		BitmapImage img;
 		Windows.Storage.StorageFile archivo;
+		Windows.Storage.StorageFolder folder;
 		public MainPage()
         {
+
             this.InitializeComponent();
 			seleccionadorImagen = new FileOpenPicker();
 			seleccionadorImagen.ViewMode = Windows.Storage.Pickers.PickerViewMode.List;
 			seleccionadorImagen.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.PicturesLibrary;
 			seleccionadorImagen.FileTypeFilter.Add(".jpg");
+			
 		}
 
 
-		async private void MButton_Click(object sender, RoutedEventArgs e)
+
+		async public void MButton_Click(object sender, RoutedEventArgs e)
 		{
 		img = new BitmapImage();
 		archivo = await seleccionadorImagen.PickSingleFileAsync();
 		IRandomAccessStream fileStream = await archivo.OpenAsync(FileAccessMode.Read);
+		
+		ListFiles(archivo);
 		await img.SetSourceAsync(fileStream);
 		MImagen.Source = img;
+			MImgDate.Text =$"{archivo.DateCreated.DayOfWeek.ToString()} " +
+				$"{ archivo.DateCreated.Day} del { archivo.DateCreated} " +
+				$"{ archivo.DateCreated.Year} ";
+
+		}
+
+		 public async void ListFiles(StorageFile ar) {
+
+
+
 		}
 
 
