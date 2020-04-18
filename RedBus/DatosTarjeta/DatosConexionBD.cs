b1556c2610d260380114720a6a4214406ad7508d
Ezequiel Monforte <ezequielmonforte@gmail.com>
2020-04-18 15:25:17 -0300

    Programacion 2 EFI 2019

---
+++ RedBus/DatosTarjeta/DatosConexionBD.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Data
+{
+    public class DatosConexionBD
+    {
+        public SqlConnection conexion;
+
+        public DatosConexionBD()
+        {
+             conexion = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Tarje;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+        public void Abrirconexion()
+        {
+            try
+            {
+                if (conexion.State == ConnectionState.Broken || conexion.State == ConnectionState.Closed)
+                {
+                    conexion.Open();
+                }
+                   
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error al abrir la conexión", e);
+            }
+        }
+
+        public void Cerrarconexion()
+        {
+            try
+            {
+                if (conexion.State == ConnectionState.Open)
+                { conexion.Close(); }
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error al cerrar la conexión", e);
+            }
+        }
+    }
+
+}
---
+++ RedBus/Entidades/Tarjeta.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+        public class Tarjeta
+        {
+            #region atributos         
+            private int nrotarjeta;
+            private int saldo;
+
+            #endregion
+
+            #region Constructor         
+            public Tarjeta()
+            {
+                nrotarjeta = 0;
+                saldo = 0;
+            }
+            
+        
+            #endregion
+
+            #region Prop        
+            public int NroTarjeta { get =>nrotarjeta; set  => nrotarjeta = value;  }
+            public int Saldo { get => saldo;  set => saldo = value;  }
+
+            #endregion
+        
+        
+            #region Metodos
+            public int Sumar(int monto)
+            {
+                saldo += monto;
+                return saldo;  
+            }
+            
+            public int Restar(int monto)
+            {
+            saldo -= monto;
+            return monto;
+            }
+            #endregion
+        }
+
+
+}
+
